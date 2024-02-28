8b0a68b94f1fda67c929324800942ddf36ea955f
quentin.boulet <56744682+Uniffay@users.noreply.github.com>
2024-02-28 21:12:43 +0100

    Front One Review

--- src/main/java/fr/uge/revue/dto/review/ReviewOneReviewDTO.java
+++ src/main/java/fr/uge/revue/dto/review/ReviewOneReviewDTO.java
@@ -5,17 +5,19 @@ import fr.uge.revue.dto.user.UserDTO;
 import fr.uge.revue.model.Comment;
 import fr.uge.revue.model.Review;
 
+import java.util.Calendar;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
-public record ReviewOneReviewDTO(long id, String title, String code, String test, int likes, UserDTO author, List<CommentDTO> comments) {
+public record ReviewOneReviewDTO(long id, String title, String commentary, String code, String test, int likes, UserDTO author, Date date, List<CommentDTO> comments) {
     public static ReviewOneReviewDTO from(Review review) {
         var comments = review.getComments()
                 .stream()
                 .sorted(Comparator.comparing(Comment::getDate))
                 .map(CommentDTO::from)
                 .toList();
-        return new ReviewOneReviewDTO(review.getId(), review.getTitle(), review.getCode(), review.getTest(), review.getLikes(),
-                UserDTO.from(review.getAuthor()), comments);
+        return new ReviewOneReviewDTO(review.getId(), review.getTitle(), review.getCommentary(), review.getCode(), review.getTest(), review.getLikes(),
+                UserDTO.from(review.getAuthor()), review.getDate(), comments);
     }
 }
