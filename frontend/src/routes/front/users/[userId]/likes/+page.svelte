b68b3b8ae14715af67260b9a87024317f8615afe
quentin.boulet <56744682+Uniffay@users.noreply.github.com>
2024-03-10 17:10:43 +0100

    html like revue, comment, response from user

--- frontend/src/routes/front/users/[userId]/likes/+page.svelte
+++ frontend/src/routes/front/users/[userId]/likes/+page.svelte
@@ -2,6 +2,7 @@
     export let data;
 
     import { authToken } from '$lib/auth';
+    import { formatDate } from '$lib/utils';
     import NavBar from '$lib/components/NavBar.svelte';
 
     const isAuthenticated = authToken.get() != null;
@@ -14,9 +15,19 @@
     <ul class="list-group mb-3">
         {#each data.likes as like}
             <li class="list-group-item position-relative">
-                <a href="/front/reviews/{like.reviewId}#{like.className}_{like.id}" class="stretched-link">
-                    {JSON.stringify(like)}
-                </a>
+                <div class="row">
+                    <a href="/front/reviews/{like.reviewId}#{like.className}_{like.id}" class="col-9">
+                        <div>
+                            <p>{like.content}</p>
+                        </div>
+                    </a>
+                    <div class="col-3" style="text-align:end;">
+                        <a href="/front/users/{like.author.id}">
+                            <p>{like.author.username}</p>
+                        </a>
+                        <p> {formatDate(new Date(like.date))}</p>
+                    </div>
+                </div>
             </li>
         {/each}
     </ul>
--- frontend/src/routes/front/users/[userId]/reviews/+page.svelte
+++ frontend/src/routes/front/users/[userId]/reviews/+page.svelte
@@ -3,6 +3,7 @@
 
     import { goto } from '$app/navigation';
     import { authToken } from '$lib/auth';
+    import { formatDate } from '$lib/utils';
     import NavBar from '$lib/components/NavBar.svelte';
 
     const isAuthenticated = authToken.get() != null;
@@ -15,9 +16,19 @@
     <ul class="list-group mb-3">
         {#each data.reviews as review}
             <li class="list-group-item position-relative">
-                <a href="/front/reviews/{review.id}" class="stretched-link">
-                    {JSON.stringify(review)}
-                </a>
+                <div class="row">
+                    <a href="/front/reviews/{review.id}" class="col-9">
+                        <div>
+                            <p>{review.title}</p>
+                        </div>
+                    </a>
+                    <div class="col-3" style="text-align:end;">
+                        <a href="/front/users/{review.author.id}">
+                            <p>{review.author.username}</p>
+                        </a>
+                        <p> {formatDate(new Date(review.date))}</p>
+                    </div>
+                </div>
             </li>
         {/each}
     </ul>
